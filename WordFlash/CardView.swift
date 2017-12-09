6de65da5017a3c12cfae30f66576399f70f9acca
Artemii Shabanov <artemiishabanov@10-254-23-234.wlan.vsu.ru>
2017-12-09 16:29:59 +0300

    Add star button

--- WordFlash/CardView.swift
+++ WordFlash/CardView.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
+protocol StarPressedDelegate {
+    func starPressed(for word:String)
+}
+
 class CardView: UIView {
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var starButton: UIButton!
+    @IBOutlet weak var definitionLabel: UILabel!
+    
+    var delegate:StarPressedDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,6 +38,25 @@ class CardView: UIView {
         contentView.backgroundColor = UIColor(red: 0.0353, green: 0.0784, blue: 0.1176, alpha: 1.0)
         contentView.layer.borderColor = (UIColor(red:255.0,green:255.0,blue:255.0,alpha:1.0)).cgColor
         wordLabel.textColor = UIColor.white
+        definitionLabel.isHidden = true
+        definitionLabel.textColor = .white
+        let gesture = UITapGestureRecognizer(target: self, action:  #selector (self.tapped (_:)))
+        self.contentView.addGestureRecognizer(gesture)
+    }
+    
+    @objc func tapped(_ sender:UITapGestureRecognizer) {
+        definitionLabel.isHidden = !definitionLabel.isHidden
+    }
+    
+    public func construct(for word:Word) {
+        wordLabel.text = word.word
+        starButton.tintColor = word.isFavorite ? .yellow : .gray
+        definitionLabel.text = word.defenition
+    }
+    
+    @IBAction func starPressed(_ sender: Any ) {
+        starButton.tintColor = starButton.tintColor == .gray ? .yellow : .gray
+        delegate?.starPressed(for: wordLabel.text!)
     }
 }
 
