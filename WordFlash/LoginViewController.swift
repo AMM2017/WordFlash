1584cda2c44d497e17fb911c7b16506c5dd977af
Artemii <artemshabanov7@mail.ru>
2017-12-19 15:19:07 +0300

    Add new pictures

--- WordFlash/LoginViewController.swift
+++ WordFlash/LoginViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginTextField.layer.borderColor = (UIColor.white).cgColor
+        passwordTextField.layer.borderColor = (UIColor.white).cgColor
+    }
+    
+    
     @IBAction func back(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
---
+++ WordFlash/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  WordFlash
+//
+//  Created by Artemii Shabanov on 19.12.2017.
+//  Copyright © 2017 Alexandr. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
