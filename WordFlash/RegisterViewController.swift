4a7bccef5405c9db0c1ec134aca94c06e15689f3
alena.pominova@gmail.com <alena.pominova@gmail.com>
2017-12-19 17:04:47 +0300

    Add constrains

--- WordFlash/RegisterViewController.swift
+++ WordFlash/RegisterViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var firstPasswordTextField: UITextField!
+    @IBOutlet weak var secondPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameTextField.layer.borderColor = (UIColor.white).cgColor
+        loginTextField.layer.borderColor = (UIColor.white).cgColor
+        firstPasswordTextField.layer.borderColor = (UIColor.white).cgColor
+        secondPasswordTextField.layer.borderColor = (UIColor.white).cgColor        
+    }
     
     @IBAction func back(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
